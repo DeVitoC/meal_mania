c0c3903601beed8241e49aafd6faa7b2aeccf591
Christopher DeVito <christopher.devito@protonmail.com>
2023-04-28 21:03:29 -0400

    Styled meals and added axios network calls

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,21 @@
 // import { useContext } from "react";
 // import { AppContext } from "../context";
 import { useGlobalContext } from "../context";
+import {BsHandThumbsUp} from "react-icons/bs";
 
 const Meals = () => {
-  const { meals } = useGlobalContext();
+  const { meals, loading } = useGlobalContext();
+
+  if (loading) {
+    return <section className="section">
+      <h4>Loading...</h4>
+    </section>
+  }
+  if (meals.length < 1) {
+    return <section className="section">
+      <h4>No meals matched your search term. Please try again.</h4>
+    </section>
+  }
 
   return (
     <section className="section-center">
@@ -13,7 +25,7 @@ const Meals = () => {
           <img src = {image} className="img" alt="meal"/>
           <footer>
             <h5>{title}</h5>
-            <button className="like-btn">Click me</button>
+            <button className="like-btn"><BsHandThumbsUp /></button>
           </footer>
         </article>
       })}
--- src/context.jsx
+++ src/context.jsx
@@ -8,15 +8,26 @@ const allMealsURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
 const randomMealURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
   const [meals, setMeals] = useState([]);
 
   const fetchMeals = async (url) => {
+    setLoading(true);
+
     try {
       const { data } = await axios(url);
-      setMeals(data.meals);
+
+      if(data.meals) {
+        setMeals(data.meals);
+      } else {
+        setMeals([]);
+      }
+
     } catch (error) {
       console.log(error.response);
     }
+
+    setLoading(false);
   }
 
   // const fetchData = async () => {
@@ -33,7 +44,7 @@ const AppProvider = ({ children }) => {
     fetchMeals(allMealsURL);
   },[])
 
-  return <AppContext.Provider value={{meals}}>
+  return <AppContext.Provider value={{meals, loading}}>
     { children }
   </AppContext.Provider>
 }
